b50cdd04297ecb92da31d755c1b4bc7d4590481c
soyKian <kianafsharinejad@gmail.com>
2023-04-22 16:18:11 -0300

    Desafio2

--- Desafio2/index.js
+++ Desafio2/index.js
@@ -1,73 +1,175 @@
+const fs = require("fs");
 class ProductManager {
+  #lastId = 0;
+
   constructor() {
-    this.products = [];
+    this.path = "./productos.json";
   }
+  
 
-  getProducts() {
-    return this.products;
-  }
-
-  addProduct(title, description, price, thumbnail, stock = 200) {
-    const product = {
-      id: this.newId() + 1,
-      title,
-      description,
-      price,
-      thumbnail,
-      stock,
-    };
-
-    if ((!title, !description, !price, !thumbnail)) {
-      return console.error(
-        "¡Todos los campos son obligatorios!, No se añadio el producto ",
-        title
-      );
-    } else {
-      return this.products.push(product);
+  async getProducts() {
+    try {
+      if (fs.existsSync(this.path)) {
+        const products = await fs.promises.readFile(this.path, "utf-8");
+        const productsJS = JSON.parse(products);
+        return productsJS;
+      } else {
+        return [];
+      }
+    } catch (error) {
+      console.log(error);
     }
   }
 
-  newId() {
-    let min = 0;
-    this.products.map((product) => {
-      if (product.id > min) {
-        min = product.id;
+  //añadir productos
+  async addProduct(title, description, price, thumbnail, stock = 200, code) {
+    try {
+      if (!title || !description || !price || !thumbnail || !code) {
+        return console.error(
+          "¡Todos los campos son obligatorios!, No se añadio el producto ",
+          title
+        );
+      } else {
+        const productFile = await this.getProducts();
+        const existCode = productFile.find((product) => product.code === code);
+
+        if (!existCode) {
+          const product = {
+            id: this.#newId(),
+            title,
+            description,
+            price,
+            thumbnail,
+            stock,
+            code,
+          };
+
+          productFile.push(product);
+          await fs.promises.writeFile(this.path, JSON.stringify(productFile));
+        } else {
+          console.log(`Ya existe un producto con el código ${code}`);
+        }
       }
-    });
-    return min;
+    } catch (error) {
+      console.log(error);
+    }
   }
 
-  getProductById(id) {
-    const searchId = this.products.find((product) => product.id === id);
-
-    return !searchId
-      ? console.error("¡Producto no encontrado!")
-      : console.log("¡Producto encontrado!: ", searchId);
+  #newId() {
+    this.#lastId++;
+    return this.#lastId;
   }
-}
+
+  //obtener productos por id
+  async getProductById(id) {
+    try {
+      const productFile = await this.getProducts();
+      const productFind = productFile.find((product) => product.id === id);
+
+      return !productFind
+        ? console.error(`¡Producto con id ${id} no encontrado!`)
+        : console.log(`¡Producto con id ${id} encontrado!:`, productFind);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  //actualizar productos
+  async updateProduct(productId, updateFile) {
+    try {
+      const productFile = await this.getProducts();
+      const findProduct = productFile.findIndex(
+        (product) => product.id === productId
+      );
+
+      if (findProduct !== -1) {
+        const updateData = {
+          ...productFile[findProduct],
+          ...updateFile,
+          id: productId,
+        };
+        productFile[findProduct] = updateData;
+        await fs.promises.writeFile(this.path, JSON.stringify(productFile));
+        console.log("¡Se realizo los cambios de forma exitosa: ", updateData);
+      } else {
+        console.log(`¡Producto ID ${productId} no existe.`);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  //Eliminar productos
+  async deleteProduct(productId) {
+    try {
+      const productFile = await this.getProducts();
+      const findProduct = productFile.findIndex(
+        (product) => product.id === productId
+      );
+
+      if (findProduct !== -1) {
+        productFile.splice(findProduct, 1);
+        await fs.promises.writeFile(this.path, JSON.stringify(productFile));
+        console.log(`Se elimino el producto id `,{productId});
+      } else {
+        console.log(`El producto id ${productId} no existe`);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+} //fin productmanager
 
 const productos = new ProductManager();
 
 console.log(productos.getProducts());
 
-productos.addProduct(
-  "Louis Vuitton Bag",
-  "LV N41358 Neverfull MM",
-  2030,
-  "https://us.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton-neverfull-mm-damier-ebene-handbags--N41358_PM2_Front%20view.jpg",
-  5
-);
+async function prueba() {
+  const consulta = await productos.getProducts();
+  console.log("La consulta del array de productos dio: ", consulta);
 
-productos.addProduct(
-  "Louis Vuitton Backpack",
-  "LV M46237 Ellipse Backpack",
-  3300,
-  "https://us.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton-ellipse-backpack--M46237_PM2_Front%20view.jpg",
-  2
-);
+  productos.addProduct(
+    "Louis Vuitton Bag",
+    "LV N41358 Neverfull MM",
+    2030,
+    "https://us.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton-neverfull-mm-damier-ebene-handbags--N41358_PM2_Front%20view.jpg",
+    5,
+    "A267X",
+  );
 
-productos.addProduct("Louis Vuitton Shoes", "", 5000, "", 0);
+  productos.addProduct(
+    "Louis Vuitton Backpack",
+    "LV M46237 Ellipse Backpack",
+    3300,
+    "https://us.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton-ellipse-backpack--M46237_PM2_Front%20view.jpg",
+    2,
+    "A19DAS",
+  );
 
-console.log(productos.getProducts());
-productos.getProductById(2);
-productos.getProductById(3);
+  const consulta2 = await productos.getProducts();
+  console.log("La 2da consulta del array con productos añadidos dio: ", consulta2);
+
+  //corroboracion de existencia de productos
+  await productos.getProductById(1);
+  await productos.getProductById(2);
+  await productos.getProductById(5);
+
+  //se actualizan productos
+  await productos.updateProduct(1, {stock:0})
+  const consulta3 = await productos.getProducts();
+  console.log("La 3ra consulta del array con productos actualizados dio: ", consulta3);
+
+  //se remueve un producto
+  await productos.deleteProduct(2)
+  const consulta4 = await productos.getProducts();
+  console.log("La 3ra consulta del array con productos actualizados dio: ", consulta4);
+
+
+  // productos.addProduct("Louis Vuitton Shoes", "", 5000, "", 0);
+
+  // console.log(productos.getProducts());
+  // productos.getProductById(2);
+  // productos.getProductById(3);
+}
+
+prueba();
